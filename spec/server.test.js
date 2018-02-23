e7635b34a0a93785151c6d3e9e43adfa553f96e8
Namson Leung <leungnamson@gmail.com>
2018-02-22 19:25:58 -0800

    changed variable name from server to app in file

--- spec/server.test.js
+++ spec/server.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
-const server = require('../server/app.js');
+const app = require('../server/app.js');
 
 describe('should send back a 200 if static file is found', () => {
   test('It should respond with a 200', (done) => {
-    request(server).get('/').then((response) => {
+    request(app).get('/').then((response) => {
       expect(response.statusCode).toBe(200);
       done();
     });
@@ -12,7 +12,7 @@ describe('should send back a 200 if static file is found', () => {
 
 describe('should receive a 404 if route isnt correct', () => {
   test('should receive a 404 from bad url', (done) => {
-    request(server).get('/gibberish').then((response) => {
+    request(app).get('/gibberish').then((response) => {
       expect(response.statusCode).toBe(404);
       done();
     });
