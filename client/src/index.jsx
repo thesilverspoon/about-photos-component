0c261aadeb38abe4990600aa159bbd87115391fd
Namson Leung <leungnamson@gmail.com>
2018-02-26 17:23:35 -0800

    passed in sample data as props to component, set state to each property in sample data

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ajax from './ajax.js';
+import sampleData from '../../data/sampleData.js';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: props.data.name,
+      description: props.data.about.description,
+      phone: props.data.about.phone,
+      price: props.data.about.price,
+      style: props.data.about.style,
+      hours: props.data.about.hours,
+      banner: props.data.banner,
+      photo: props.data.photo,
+    };
   }
 
   render() {
@@ -13,4 +25,4 @@ export default class App extends React.Component {
   }
 }
 
-render(<App />, document.getElementById('app'));
+render(<App data={sampleData[0]} />, document.getElementById('app'));
