717ce67d5393fcb9f6e9c2308c562b3bf09fd9db
Namson Leung <leungnamson@gmail.com>
2018-02-28 15:52:33 -0800

    passed data into main component, set state to appropriate properties

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,15 +8,44 @@ import About from './About.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      id: props.data.id,
+      name: props.data.name,
+      description: props.data.about.description,
+      hours: props.data.about.hours,
+      phone: props.data.about.phone,
+      price: props.data.about.price,
+      style: props.data.about.style,
+      banner: props.data.banner,
+      photo: props.data.photo,
+    }
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  fetch() {
+    ajax.get((err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        this.setState({
+          restaurant: data.restaurant,
+        })
+      }
+    });
   }
 
   render() {
     return (
       <div>
-        <About data={sampleData[0]} />
+        <About />
       </div>
     );
   }
 }
+
+render(<App data={sampleData[0]}/>, document.getElementById('app'));
 
 export default App;
