679e4ab187ef88966c431c2f10d0d2f357c0cb64
Namson Leung <leungnamson@gmail.com>
2018-03-01 13:02:19 -0800

    modified/updated tests

--- spec/About.test.js
+++ spec/About.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow } from 'enzyme';
+import sample from '../data/sampleData.js';
 
 import About from '../client/src/About.jsx';
 
@@ -9,13 +10,13 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('About Component', () => {
   test('should render correctly', () => {
-    expect(shallow(<About />)).toMatchSnapshot();
+    expect(shallow(<About photo={sample[0].photo} />)).toMatchSnapshot();
   });
 });
 
 describe("rendered `About`", () => {
-  it("received two props", () => {
-    const component = shallow(<About />);
-    expect(component.props().children.length).toBe(2);
+  it("received three props", () => {
+    const component = shallow(<About photo={sample[0].photo} />);
+    expect(component.props().children.length).toBe(3);
   });
 });
