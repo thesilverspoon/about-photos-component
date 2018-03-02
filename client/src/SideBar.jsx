673ddfe33c0f12cb8f70473e98af773cad04cf1c
Namson Leung <leungnamson@gmail.com>
2018-03-01 16:28:39 -0800

    added clock icon

--- client/src/SideBar.jsx
+++ client/src/SideBar.jsx
@@ -4,18 +4,24 @@ const SideBar = ({ price, hours, style, phone }) => {
   return (
     <div>
       <img src="https://s3-us-west-1.amazonaws.com/imagesfornamson/21.png" className="moneyIcon" />
-      <div className="side">Price: 
-        <div className="sideInfo">{price}</div>
+      <div> 
+        <div className="side">Price:</div>
+        <div className="sideInfoPrice">{price}</div>
       </div>
-      <div className="side">Hours of Operation:
-        <div className="sideInfo">{hours}</div>
+      <div> 
+        <div className="side">Hours of Operation:</div>
+        <img src="https://s3-us-west-1.amazonaws.com/imagesfornamson/clock.png" className="clockIcon" />
+        <div className="sideInfoHours">{hours}</div>
       </div>
-      <div className="side">Dress code:
-        <div className="sideInfo">{style}</div>
+      <div>
+        <div className="side">Dress code:</div>
+        <img src="https://s3-us-west-1.amazonaws.com/imagesfornamson/img_473372.png" className="dressIcon" />
+        <div className="sideInfoStyle">{style}</div>
       </div>
-      <div className="side">Phone Number:
+      <div>
+        <div className="side">Phone Number:</div>
         <img src="https://s3-us-west-1.amazonaws.com/imagesfornamson/telephone_318-61547.jpg" className="phoneIcon" />
-        <div className="sideInfo">{phone}</div>
+        <div className="sideInfoPhone" >{phone}</div>
       </div>
     </div>
   );
