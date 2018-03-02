07193b65b3ffadb43d9e1e132264b8094723456a
Namson Leung <leungnamson@gmail.com>
2018-03-01 16:03:52 -0800

    added phone icon

--- client/src/SideBar.jsx
+++ client/src/SideBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SideBar = ({ price, hours, style, phone }) => {
   return (
     <div>
-      <img src="https://s3-us-west-1.amazonaws.com/imagesfornamson/21.png" className="moneyIcon"/>
+      <img src="https://s3-us-west-1.amazonaws.com/imagesfornamson/21.png" className="moneyIcon" />
       <div className="side">Price: 
         <div className="sideInfo">{price}</div>
       </div>
@@ -14,6 +14,7 @@ const SideBar = ({ price, hours, style, phone }) => {
         <div className="sideInfo">{style}</div>
       </div>
       <div className="side">Phone Number:
+        <img src="https://s3-us-west-1.amazonaws.com/imagesfornamson/telephone_318-61547.jpg" className="phoneIcon" />
         <div className="sideInfo">{phone}</div>
       </div>
     </div>
