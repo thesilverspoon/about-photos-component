26415015232ae1ad631f22d6090f4551db93baa2
Namson Leung <leungnamson@gmail.com>
2018-03-05 15:25:38 -0800

    fixed webpack prod file pointer, added script in package json file

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,7 +5,7 @@ var BUILD_DIR = path.resolve(__dirname, 'client/dist');
 var APP_DIR = path.resolve(__dirname, 'client/src');
 
 var config = {
-  entry: './client/src/productionView.jsx',
+  entry: './client/src/productionView.js',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
