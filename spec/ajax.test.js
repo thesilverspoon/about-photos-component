0e39e828248ab2641075595bffd837460e304ace
Namson Leung <leungnamson@gmail.com>
2018-03-10 10:31:08 -0800

    update test

--- spec/ajax.test.js
+++ spec/ajax.test.js
@@ -1,30 +1,29 @@
-// const ajax = require('../client/src/ajax.js');
-// const sinon = require('sinon');
-// const sampleData = require('../data/sampleData.js');
+const ajax = require('../client/src/ajax.js');
+const sinon = require('sinon');
+const sampleData = require('../data/sampleData.js');
 
-// const BASE_URL = process.env.BASE_URL ? process.env.BASE_URL : '';
+const BASE_URL = process.env.BASE_URL ? process.env.BASE_URL : '';
 
-// describe("with fake server", function() {
-//   const data = sampleData[0];
-//   let server;
+describe("with fake server", function() {
+  const data = sampleData[0];
+  let server;
 
-//   beforeEach(function () {
-//     server = sinon.fakeServer.create()
-//   });
+  beforeEach(function () {
+    server = sinon.fakeServer.create()
+  });
 
-//   afterEach(function () {
-//     server.restore();
-//   });
+  afterEach(function () {
+    server.restore();
+  });
 
-//   it("fetches with the correct id and url", function () {
-//     server.respondWith("GET", `${BASE_URL}/restaurants/90976`, [200, {"Content-Type":"application/json"}, JSON.stringify(data)]);
-//     const callback = jasmine.createSpy('callback');
-//     ajax.get(90976, callback);
-//     server.respond();
-//     var res = callback.calls.mostRecent().args[1];
-//     expect(callback).toHaveBeenCalled();
-//     expect(res.id).toBe(90976);
-//     expect(res.name).toBe("Beard Papa's");
-//   });
-// });
-
+  // it("fetches with the correct id and url", function () {
+  //   server.respondWith("GET", `${BASE_URL}/restaurants/90976`, [200, {"Content-Type":"application/json"}, JSON.stringify(data)]);
+  //   const callback = jasmine.createSpy('callback');
+  //   ajax.get(90976, callback);
+  //   server.respond();
+  //   var res = callback.calls.mostRecent().args[1];
+  //   expect(callback).toHaveBeenCalled();
+  //   expect(res.id).toBe(90976);
+  //   expect(res.name).toBe("Beard Papa's");
+  // });
+});
