0e9fd3a8d4ee80de0c40e32acc2a864650815f1c
Namson Leung <leungnamson@gmail.com>
2018-03-10 10:19:09 -0800

    updated css, updated local version of this repo

--- client/src/Banner.jsx
+++ client/src/Banner.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Banner = ({ banner }) => {
   return (
     <div>
-      <img className="bannerImg" src={banner} />
+      <img className="bannerImg" src={banner[0]} />
+      <img className="bannerImg" src={banner[1]} />
+      <img className="bannerImg" src={banner[2]} />
+      <img className="bannerImg" src={banner[3]} />
+
     </div>
   );
 };
